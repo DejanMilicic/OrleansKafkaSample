2b208a2ab891f5d4f38aa1c0335f00160b1e55ce
Dejan Miličić <dejan.milicic@gmail.com>
2025-04-05 17:11:04 +0200

    fix

--- Grains/GreetingGrain.fs
+++ Grains/GreetingGrain.fs
@@ -4,7 +4,12 @@ open System.Threading.Tasks
 open Orleans
 open System
 
-[<Serializable>]
+// following three lines are necessary to make the Orleans codegen work
+// "CodeGen" is the name of the C# codegen project
+open System.Runtime.CompilerServices
+[<assembly: InternalsVisibleTo("CodeGen")>]
+do()
+
 [<GenerateSerializer>]
 type GreeterMessage =
     | Greeting of string
@@ -21,6 +26,6 @@ type GreetingGrain() =
         member this.Receive(msg: GreeterMessage) =
             match msg with
             | Greeting greeting ->
-                Task.FromResult($"Greeter received greeting: {greeting}")
+                Task.FromResult($"Greeter received greeting: *{greeting}*")
             | Number number ->
                 Task.FromResult($"Greeter received number: {number}")
